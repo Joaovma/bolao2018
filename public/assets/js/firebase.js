35a06b76fdd0217e9221918a1b9a64075b8c635e
Henrique Brighenti <hbrighen@thoughtworks.com>
2018-05-27 18:41:37 -0300

    Salva apostas de primeira etapa no banco

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -1,39 +1,46 @@
-
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyD-b-FKQceXUbjFCh3XSkaqW1OV5ZZh-PM",
-	authDomain: "bolao2018copadomundo.firebaseapp.com",
-	databaseURL: "https://bolao2018copadomundo.firebaseio.com",
-	projectId: "bolao2018copadomundo"
+    apiKey: "AIzaSyD-b-FKQceXUbjFCh3XSkaqW1OV5ZZh-PM",
+    authDomain: "bolao2018copadomundo.firebaseapp.com",
+    databaseURL: "https://bolao2018copadomundo.firebaseio.com",
+    projectId: "bolao2018copadomundo"
 };
 firebase.initializeApp(config);
 
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (!user) {
-    	if (window.location.pathname != '/index.html' && window.location.pathname != '/') {
-    		window.location = 'index.html';
-    	}
+        if (window.location.pathname != '/index.html' && window.location.pathname != '/') {
+            window.location = 'index.html';
+        }
     }
 });
 
 var databaseBolao = firebase.database();
 
 function firebaseSignOut() {
-	firebase.auth().signOut();
+    firebase.auth().signOut();
 }
 
-function firebaseGetUserData(){
-	return new Promise (function (resolve, reject){
-		firebase.auth().onAuthStateChanged(function (user){
-			const UID = user.uid;
-			databaseBolao.ref('usuarios/'+ UID).once('value', function(snap){
-				resolve(snap.val());
-			}, function (errorObject) {
-				console.error("CAGOU A PORRA TODA - Read failed" + errorObject);
-				reject(errorObject);
-			});
-		}); 
-		
-	});
+function firebaseGetUserData() {
+    return new Promise(function (resolve, reject) {
+        firebase.auth().onAuthStateChanged(function (user) {
+            const UID = user.uid;
+            databaseBolao.ref('usuarios/' + UID).once('value', function (snap) {
+                resolve(snap.val());
+            }, function (errorObject) {
+                console.error("CAGOU A PORRA TODA - Read failed" + errorObject);
+                reject(errorObject);
+            });
+        });
+    });
+}
+
+function firebaseSaveBet(aposta) {
+    const UID = firebase.auth().currentUser.uid;
+    if (!UID) {
+        return;
+    }
+
+    databaseBolao.ref('apostas/' + UID + '/etapa1').set(aposta);
 }
