23cb3974462ef41925bbb1868eb771d60ba52beb
Henrique Brighenti <hbrighen@thoughtworks.com>
2018-06-02 16:55:01 -0300

    Primeira versao da funcao de calculo de pontuacao

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+var db = admin.database();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.calculaPontuacao = functions.database.ref('/oficial').onWrite((change, context) => {
+    const oficial = change.after.val();
+    return db.ref('apostas').once('value', (snap) => {
+        snap.forEach((childNode) => {
+            var aposta = childNode.val();
+            var pontuacaoFaseGrupos = calculaFaseGrupos(oficial, aposta);
+            var pontuacaoOitavas = 0;
+            var pontuacaoQuartas = 0;
+            var pontuacaoSemi = 0;
+            var pontuacaoTerceiro = 0;
+            var pontuacaoFinal = 0;
+            var pontuacaoTotal = pontuacaoFaseGrupos + pontuacaoOitavas + pontuacaoQuartas +
+                pontuacaoSemi + pontuacaoTerceiro + pontuacaoFinal;
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+                db.ref('usuarios/' + childNode.key).update({
+                pontuacaoFaseGrupos,
+                pontuacaoOitavas,
+                pontuacaoQuartas,
+                pontuacaoSemi,
+                pontuacaoTerceiro,
+                pontuacaoFinal,
+                pontuacaoTotal
+            });
+        });
+    });
 });
+
+function calculaFaseGrupos(oficial, usuario) {
+    return 10;
+}
